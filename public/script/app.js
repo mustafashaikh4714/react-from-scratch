8dc83a445ca630b7c8a443c17bc40170e858c169
mustafashaikh4714 <shaikhmustafa.4714@gmail.com>
2019-08-07 13:49:37 +0530

    conditional rendering using functions, ternary operators and logical AND operators

--- public/script/app.js
+++ public/script/app.js
@@ -2,5 +2,65 @@
 
 console.log('Inside app JSX');
 
+var app = {
+  title: 'Make ToDo',
+  subtitle: 'Put your life in the hands of computer!'
+};
+
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  )
+);
+
+var user = {
+  name: 'Mustafa',
+  age: 20,
+  location: ''
+};
+
+function getLocation(location) {
+  if (location) return React.createElement(
+    'p',
+    null,
+    'Location: ',
+    location
+  );
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age >= 18 && React.createElement(
+    'p',
+    null,
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    getLocation(user.location)
+  )
+);
+
 var appRoute = document.getElementById('app');
 ReactDOM.render(templateTwo, appRoute);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,35 @@
 console.log('Inside app JSX')
 
+let app = {
+  title: 'Make ToDo',
+  subtitle: 'Put your life in the hands of computer!'
+}
+
+let template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+  </div>
+)
+
+let user = {
+  name: 'Mustafa',
+  age: 20,
+  location: ''
+}
+
+function getLocation(location) {
+  if (location) return <p>Location: {location}</p>
+}
+
+let templateTwo = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{user.name ? user.name : 'Anonymous'}</p>
+    {user.age >= 18 && <p>{user.age}</p>}
+    <p>{getLocation(user.location)}</p>
+  </div>
+)
+
 let appRoute = document.getElementById('app')
 ReactDOM.render(templateTwo, appRoute)
